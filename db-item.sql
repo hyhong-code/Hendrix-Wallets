46302c648d38320610265054f28b712895dc1566
Haiyang Hong <hong961127@gmail.com>
2020-07-16 00:26:11 -0700

    Add items data to dab

---
+++ db-item.sql
@@ -0,0 +1,95 @@
+-- Insert Items
+INSERT INTO
+  items(
+    name,
+    category_id,
+    description,
+    photo,
+    price,
+    discount
+  )
+VALUES
+  (
+    'YORBAX',
+    '75863da8-4f63-4ba1-bd74-e2f87c7ca4ac',
+    'adipiscing tristique risus nec feugiat in fermentum posuere urna nec tincidunt praesent semper feugiat nibh',
+    'https://haiyanghongnewbucket.s3-us-west-2.amazonaws.com/hendrix/item/yorbax.jpg',
+    '1699',
+    '500'
+  );
+
+INSERT INTO
+  items(
+    name,
+    category_id,
+    description,
+    photo,
+    price,
+    discount
+  )
+VALUES
+  (
+    'TIGMON',
+    '75863da8-4f63-4ba1-bd74-e2f87c7ca4ac',
+    'neque viverra justo nec ultrices dui sapien eget mi proin sed libero enim sed faucibus',
+    'https://haiyanghongnewbucket.s3-us-west-2.amazonaws.com/hendrix/item/tigmon.jpg',
+    '1699',
+    '500'
+  );
+
+INSERT INTO
+  items(
+    name,
+    category_id,
+    description,
+    photo,
+    price,
+    discount
+  )
+VALUES
+  (
+    'FORROLY',
+    '75863da8-4f63-4ba1-bd74-e2f87c7ca4ac',
+    'diam maecenas sed enim ut sem viverra aliquet eget sit amet tellus cras adipiscing enim',
+    'https://haiyanghongnewbucket.s3-us-west-2.amazonaws.com/hendrix/item/forroly.jpg',
+    '1599',
+    '300'
+  );
+
+INSERT INTO
+  items(
+    name,
+    category_id,
+    description,
+    photo,
+    price,
+    discount
+  )
+VALUES
+  (
+    'FONICY',
+    '75863da8-4f63-4ba1-bd74-e2f87c7ca4ac',
+    'adipiscing elit ut aliquam purus sit amet luctus venenatis lectus magna fringilla urna porttitor rhoncus',
+    'https://haiyanghongnewbucket.s3-us-west-2.amazonaws.com/hendrix/item/fonicy.jpg',
+    '1499',
+    '300'
+  );
+
+INSERT INTO
+  items(
+    name,
+    category_id,
+    description,
+    photo,
+    price,
+    discount
+  )
+VALUES
+  (
+    'DOZZBY',
+    '75863da8-4f63-4ba1-bd74-e2f87c7ca4ac',
+    'sem fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque massa placerat duis ultricies',
+    'https://haiyanghongnewbucket.s3-us-west-2.amazonaws.com/hendrix/item/dozzby.jpg',
+    '1899',
+    '200'
+  );
