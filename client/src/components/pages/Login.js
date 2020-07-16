299656ece3c3ae524abbf88dd7b6c835e443d7f5
Haiyang Hong <hong961127@gmail.com>
2020-07-16 05:26:04 -0700

    Add login form

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,10 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+  const handleChange = (evt) => {
+    const { name, value } = evt.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(formData);
+    setFormData({
+      email: "",
+      password: "",
+    });
+  };
+
   return (
-    <div>
-      <h1>login</h1>
-    </div>
+    <section id="signup" className="py-6 bg-light text-dark">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">
+            <h1 className="display-4 text-center text-primary mb-5">
+              <span>LOG IN</span>
+            </h1>
+            <div className="form">
+              <div className="card card-body py-5 px-3">
+                <div className="row">
+                  <div className="col-10 offset-1">
+                    <form onSubmit={handleSubmit}>
+                      <div className="form-group">
+                        <label htmlFor="email">EMAIL</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="email"
+                          name="email"
+                          value={email}
+                          onChange={handleChange}
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="password">PASSWORD</label>
+                        <input
+                          type="password"
+                          className="form-control"
+                          id="password"
+                          name="password"
+                          value={password}
+                          onChange={handleChange}
+                        />
+                      </div>
+
+                      <input
+                        type="submit"
+                        className="btn btn-primary btn-block"
+                        value="LOG IN"
+                      />
+                      <small className="form-text text-muted mt-3">
+                        Don't have an account?{" "}
+                        <Link to="/signup" className="text-secondary">
+                          Sign up <i className="fas fa-arrow-right"></i>
+                        </Link>
+                      </small>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- client/src/components/pages/Signup.js
+++ client/src/components/pages/Signup.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+  });
+
+  const { name, email, password, passwordConfirm } = formData;
+  const handleChange = (evt) => {
+    const { name, value } = evt.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(formData);
+    setFormData({
+      name: "",
+      email: "",
+      password: "",
+      passwordConfirm: "",
+    });
+  };
+
   return (
     <section id="signup" className="py-6 bg-light text-dark">
       <div className="container">
@@ -14,53 +38,65 @@ const Signup = () => {
               <div className="card card-body py-5 px-3">
                 <div className="row">
                   <div className="col-10 offset-1">
-                    <div className="form-group">
-                      <label htmlFor="email">NAME</label>
+                    <form onSubmit={handleSubmit}>
+                      <div className="form-group">
+                        <label htmlFor="email">EMAIL</label>
+                        <input
+                          type="email"
+                          className="form-control"
+                          id="email"
+                          name="email"
+                          value={email}
+                          onChange={handleChange}
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="name">NAME</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="name"
+                          name="name"
+                          value={name}
+                          onChange={handleChange}
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="password">PASSWORD</label>
+                        <input
+                          type="password"
+                          className="form-control"
+                          id="password"
+                          name="password"
+                          value={password}
+                          onChange={handleChange}
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="passwordConfirm">
+                          CONFIRM PASSWORD
+                        </label>
+                        <input
+                          type="password"
+                          className="form-control"
+                          id="passwordConfirm"
+                          name="passwordConfirm"
+                          value={passwordConfirm}
+                          onChange={handleChange}
+                        />
+                      </div>
                       <input
-                        type="email"
-                        className="form-control"
-                        id="email"
-                        name="email"
+                        type="submit"
+                        className="btn btn-primary btn-block"
+                        value="SIGN UP"
                       />
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="name">EMAIL</label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        id="name"
-                        name="name"
-                      />
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="password">PASSWORD</label>
-                      <input
-                        type="password"
-                        className="form-control"
-                        id="password"
-                        name="password"
-                      />
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="passwordConfirm">CONFIRM PASSWORD</label>
-                      <input
-                        type="password"
-                        className="form-control"
-                        id="passwordConfirm"
-                        name="passwordConfirm"
-                      />
-                    </div>
-                    <input
-                      type="submit"
-                      className="btn btn-primary btn-block"
-                      value="SIGN UP"
-                    />
-                    <small className="form-text text-muted mt-3">
-                      Already have account?{" "}
-                      <Link to="/login">
-                        Log in <i className="fas fa-arrow-right"></i>
-                      </Link>
-                    </small>
+                      <small className="form-text text-muted mt-3">
+                        Already have an account?{" "}
+                        <Link to="/login" className="text-secondary">
+                          Log in <i className="fas fa-arrow-right"></i>
+                        </Link>
+                      </small>
+                    </form>
                   </div>
                 </div>
               </div>
