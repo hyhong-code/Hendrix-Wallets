23f78231c96935c6b172659bd86783d72f2f7be4
Haiyang Hong <hong961127@gmail.com>
2020-07-18 12:18:34 -0700

    Equipt cateogry and items actions with alert toast

--- client/src/actions/categoryActions.js
+++ client/src/actions/categoryActions.js
@@ -1,11 +1,14 @@
 import axios from "axios";
+
 import { CATEGORIES_FETCHED, CATEGORIES_ERROR } from "./types";
+import { createToast } from "../actions/toastActions";
 
 export const getCategories = () => async (dispatch) => {
   try {
     const res = await axios.get("/api/category");
     dispatch({ type: CATEGORIES_FETCHED, payload: res.data.categories });
   } catch (error) {
-    console.error(error);
+    // console.error(error.response);
+    dispatch({ type: CATEGORIES_ERROR });
   }
 };
--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,12 +1,18 @@
 import axios from "axios";
 
 import { ITEMS_FETCHED, ITEMS_ERROR } from "./types";
+import { createToast } from "../actions/toastActions";
 
 export const getItems = () => async (dispatch) => {
   try {
     const res = await axios.get("/api/item");
     dispatch({ type: ITEMS_FETCHED, payload: res.data.items });
   } catch (error) {
-    console.error(error);
+    dispatch({ type: ITEMS_ERROR });
+    if (error.response) {
+      dispatch(
+        createToast(Object.values(error.response.data.errors).join(", "))
+      );
+    }
   }
 };
