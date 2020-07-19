4dd8c52c81edcff946d58935f6c79de72287c3f8
Haiyang Hong <hong961127@gmail.com>
2020-07-19 09:07:21 -0700

    Fix Heroku

---
+++ client/build/precache-manifest.677ddbd202a95a638bb3376984713d0a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66b701eda77c77783440f5b7ab959524",
+    "url": "/index.html"
+  },
+  {
+    "revision": "605534ee4d9f3b920360",
+    "url": "/static/css/2.8d660b98.chunk.css"
+  },
+  {
+    "revision": "f8c7fa1c4ed6477e634b",
+    "url": "/static/css/main.ad971edc.chunk.css"
+  },
+  {
+    "revision": "605534ee4d9f3b920360",
+    "url": "/static/js/2.51edb8ba.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/static/js/2.51edb8ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8c7fa1c4ed6477e634b",
+    "url": "/static/js/main.54801307.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "97a48647b51154c6044fcf3a947edefb",
+    "url": "/static/media/checkout.97a48647.jpg"
+  },
+  {
+    "revision": "9abe73633259e233608905df49438938",
+    "url": "/static/media/home.9abe7363.jpeg"
+  },
+  {
+    "revision": "47e7c15c6404e588a55207fc2bc77a62",
+    "url": "/static/media/items.47e7c15c.jpeg"
+  },
+  {
+    "revision": "4f9ae704b024e0e7def65ecb754cc519",
+    "url": "/static/media/logo.4f9ae704.png"
+  }
+]);
