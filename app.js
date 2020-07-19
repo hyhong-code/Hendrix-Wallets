bc41ccb3cb03364b0f851fd3486829a46c641697
Haiyang Hong <hong961127@gmail.com>
2020-07-19 08:42:02 -0700

    Ready for Heroku deployment

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config({ path: "./config/config.env" });
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const path = require("path");
 
 const authRouter = require("./routes/authRouter");
 const profileRouter = require("./routes/profileRouter");
@@ -16,6 +17,10 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
+
 // Mount Routers
 app.use("/api/auth", authRouter);
 app.use("/api/profile", profileRouter);
@@ -23,6 +28,10 @@ app.use("/api/category", categoryRouter);
 app.use("/api/item", itemRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
+
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, console.log(`Server up on port ${port}`));
