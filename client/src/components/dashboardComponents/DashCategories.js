c3d59a5da4e0bd0db1558a8c3e3c591ae2902a67
Haiyang Hong <hong961127@gmail.com>
2020-07-22 02:12:30 -0700

    Map category cards on dashboard categories tab

--- client/src/components/dashboardComponents/DashCategories.js
+++ client/src/components/dashboardComponents/DashCategories.js
@@ -1,16 +1,45 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const DashCategories = () => {
+const DashCategories = ({ categories, items }) => {
   return (
     <div
-      className="tab-pane fade"
+      className="tab-pane fade dash-categories"
       id="v-pills-categories"
       role="tabpanel"
       aria-labelledby="v-pills-categories-tab"
     >
-      <h1>Categories</h1>
+      {categories &&
+        items &&
+        categories.map((category) => (
+          <div class="card mb-3">
+            <div className="d-flex align-items-center">
+              <img
+                src={category.photo}
+                class="category-img"
+                alt={category.name}
+              />
+              <div class="card-body">
+                <h5 class="card-title text-primary mb-1">{category.name}</h5>
+                <div className="mb-2">
+                  <span>Collection Items Quantity: </span>
+                  <span className="badge badge-dark text-light">
+                    {items.reduce(
+                      (acc, cur) =>
+                        cur.category_id === category.id ? acc + 1 : acc,
+                      0
+                    )}
+                  </span>
+                </div>
+                <p class="card-text text-secondary">{category.description}</p>
+              </div>
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
 
-export default DashCategories;
+const mapStateToProps = ({ categories, items }) => ({ categories, items });
+
+export default connect(mapStateToProps)(DashCategories);
